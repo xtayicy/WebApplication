4f57a4c2a8b0990bdb968f23f9c4209688402fb6
xtayicy <xtayicy@gmail.com>
2022-05-21 15:21:56 +0800

    c#

---
+++ Data/PersonContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext (DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication.Models.Person> Person { get; set; }
+    }
+}
