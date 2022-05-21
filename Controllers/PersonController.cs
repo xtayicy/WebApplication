85f665c9dd26ca63d3697b6b72ff443fafd5d044
xtayicy <xtayicy@gmail.com>
2022-05-21 14:30:45 +0800

    c#

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
@@ -11,18 +12,19 @@ namespace WebApplication.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            Person[] persons = new Person[2] { new Person(1, "Mike", 22), new Person(2, "Harry", 21) };
+
+            return View(persons);
         }
 
-        /*public string ShowPerson(string name,int age) {
+        /*public string Details(string name,int age) {
 
             //return $"name:{name} age:{age}";
             return HtmlEncoder.Default.Encode($"name:{name} age:{age}");
         }*/
 
-        public IActionResult ShowPerson(string name,int age) {
-            ViewData["name"] = name;
-            ViewData["age"] = age;
+        public IActionResult Details() {
+            //Person[] persons = new Person[2] { new Person(1, "Mike", 22), new Person(2, "Harry", 21) };
 
             return View();
         }
---
+++ Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Person
+    {   
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person(int id,string name,int age) {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
